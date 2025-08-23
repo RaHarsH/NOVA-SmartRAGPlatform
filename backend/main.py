ca451115058ee5e1c634668fa02dbc429c17cf5c
harshith <harshith.raju0710@gmail.com>
2025-08-23 09:04:13 +0530

    explicitly added user-id header in the backend

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*", "Content-Type", "user-id"], # Explicitly add 'user-id'
 )
 
 app.include_router(pdf_routes.router, prefix="/api/pdf", tags=["PDF Upload"])
